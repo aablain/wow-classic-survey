0a743b6285ea99fce27de579e0b7cdf21157e215
Austin Blain <austin.a.blain@gmail.com>
2019-04-04 20:22:10 -0700

    Updates

---
+++ precache-manifest.41e63fcc6c96cc65a2aefdb6d44836a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8f7752255982c1dcd5b",
+    "url": "/wow-classic-survey/static/js/runtime~main.b163ddd3.js"
+  },
+  {
+    "revision": "adfe5d0b1adc050d40e8",
+    "url": "/wow-classic-survey/static/js/main.8f6df24c.chunk.js"
+  },
+  {
+    "revision": "532aa59cda9070bd52a8",
+    "url": "/wow-classic-survey/static/js/2.825c53e4.chunk.js"
+  },
+  {
+    "revision": "adfe5d0b1adc050d40e8",
+    "url": "/wow-classic-survey/static/css/main.9f68f35e.chunk.css"
+  },
+  {
+    "revision": "8c5e3d41f41ca316611953646ecaf223",
+    "url": "/wow-classic-survey/index.html"
+  }
+];
